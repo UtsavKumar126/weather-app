97c8d489f6a616376016fc57786ff950f5fc083e
UtsavKumar126/ <guptautsavkumar126@gmail.com>
2023-12-23 20:21:14 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import summer from"./images/summer.jpg"
+import winter from "./images/winter.jpg"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App=()=>{
+    const[longitude,setLongitude]=useState(0);
+    const[latitude,setLatitude]=useState(0);
+    const[hemisphere,setHemisphere]=useState(0);
+    const[month,setMonth]=useState(()=>{
+        return new Date().getMonth()+1
+    })
+
+    // this will call fetch function only once when the page is loaded
+    useEffect(()=>{
+        fetchLocation();
+    },[])
+
+    function fetchLocation(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(
+                (position)=>{
+                    setLongitude(position.coords.longitude);
+                    setLatitude(position.coords.latitude);
+
+
+                    if(position.coords.longitude>0){
+                        setHemisphere("Northen Hemisphere")
+                    }
+                    else if(position.coords.longitude<0){
+                        setHemisphere("Southern Hemisphere")
+                    }
+                    else{
+                        setHemisphere("Equator")
+                    }
+                }
+            )
+        }
+        else{
+            alert("Geolocation not Found")
+        }
+    }
+
+
+    return(
+        <div id="main">
+            {/* <button onClick={fetchLocation}>Fetch Location</button> */}
+            <h1>Latitude : {latitude}</h1>
+            <h1>Longitude : {longitude}</h1>
+            <h1>Hemisphere : {hemisphere}</h1>
+            <h1>Month: {month}</h1>
+            {
+                hemisphere&&((hemisphere==="Northen Hemisphere"&&month>=4&&month<=10)||
+                (hemisphere==="Southern Hemisphere"&&(month<4||month>10)))&&(
+                    <div>
+                        <h1>Summer Season</h1>
+                        <img src={summer} alt="summer"/>
+                    </div>
+                )
+            }
+            {
+                hemisphere&&((hemisphere==="Northen Hemisphere"&&(month<4||month>10))||
+                (hemisphere==="Southern Hemisphere"&&month>=4&&month<=10))&&(
+                    <div>
+                        <h1>Winter Season</h1>
+                        <img src={winter} alt="winter "/>
+                    </div>
+                )
+            }
+
+        </div>
+    )
 }
 
-export default App;
+export default App
